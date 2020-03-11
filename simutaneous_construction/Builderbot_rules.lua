431cfc96a0b451b1a466e0516849cdae1b89c508
yating1901 <47184913+yating1901@users.noreply.github.com>
2020-03-11 17:54:59 +0100

    Add files via upload

--- simutaneous_construction/Builderbot_rules.lua
+++ simutaneous_construction/Builderbot_rules.lua
@@ -21,59 +21,15 @@ rules.list = {
       structure = {
          {
             index = vector3(0, 0, 0),
-            type = 2
-         },
-         {
-            index = vector3(0, 0, 1),
             type = 3
-         },
-      },
-      target = {
-         reference_index = vector3(0, 0, 0),
-         offset_from_reference = vector3(1, 0, 0),
-         type = 3
-      },
-      generate_orientations = false
-   },
-   {
-      rule_type = 'place',
-      structure = {
-         {
-            index = vector3(0, 0, 0),
-            type = 2
-         },
-         {
-            index = vector3(0, 1, 0),
-            type = 3
-         },
-         {
-            index = vector3(0, 0, 1),
-            type = 3
-         },
-      },
-      target = {
-         reference_index = vector3(0, 0, 0),
-         offset_from_reference = vector3(1, 0, 0),
-         type = 3
-      },
-      generate_orientations = false
-   },
-   {
-      rule_type = 'place',
-      structure = {
-         {
-            index = vector3(0, 0, 0),
-            type = 2
          },
       },
       target = {
          reference_index = vector3(0, 0, 0),
          offset_from_reference = vector3(0, 0, 1),
-         type = 3
       },
       generate_orientations = false
    }
-   
 }
 rules.selection_method = 'nearest_win'
 return rules
