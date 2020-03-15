56c490b02c7f78d13ecaa84cb3439131391f0d94
yating1901 <47184913+yating1901@users.noreply.github.com>
2020-03-15 18:08:12 +0100

    Add files via upload

--- BuilderBotAPI.lua
+++ BuilderBotAPI.lua
@@ -163,13 +163,13 @@ builderbot_api.subprocess_leds = function()
             local color_number = robot.camera_system.detect_led(led_loc_for_camera)
             if color_number ~= tag.type and color_number ~= 0 then
                tag.type = color_number
-               block.type = tag.type  --need to be comment with the intelligence of block
+               --block.type = tag.type
                DebugMSG(j,'tag_type:',tag.type)
             end
          end
       end
-----------
---[[      if block.tags.up ~= nil then  
+     
+      if block.tags.up ~= nil then  
          if block.tags.up.type == 0 then
              block.type = 0
          end
@@ -181,15 +181,21 @@ builderbot_api.subprocess_leds = function()
          if block.tags.front.type == 0 then
              block.type = 0
          end
-          if block.tags.front.type == 1 then  --purple
+         if block.tags.front.type == 1 then  --purple
              block.type = 2                   --put one ahead
          end
       end
       if block.tags.up ~= nil and block.tags.front ~= nil then
          if block.tags.up.type == 3 and block.tags.front.type == 3 then
              block.type = 1
-         end          
-      end]]
+         end   
+        --[[ if block.tags.up.type == 3 and block.tags.front.type == 1 then
+             block.type = 2
+         end  
+         if block.tags.up.type == 2 and block.tags.front.type == 3 then
+             block.type = 3
+         end     --]]     
+      end
       DebugMSG(i,'block_type:',block.type)
    end
 end
